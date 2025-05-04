880b06d4b2df53e528d4d3c41877e35901892503
luxidity <sky47778@gmail.com>
2025-05-03 21:02:08 -0400

    init new docusaurus repo with search

---
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -0,0 +1,6 @@
+import lunr from "/Users/sean/Documents/GitHub/lucid/node_modules/lunr/lunr.js";
+export const removeDefaultStopWordFilter = false;
+export const language = ["en"];
+export const searchIndexUrl = "search-index{dir}.json?_=4d82099b";
+export const searchResultLimits = 8;
+export const fuzzyMatchingDistance = 1;
---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/sean/Documents/GitHub/lucid/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/sean/Documents/GitHub/lucid/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/sean/Documents/GitHub/lucid/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/sean/Documents/GitHub/lucid/src/css/custom.css"),
+];
